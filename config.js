e7dd87bdc3289e259a35664501c5465d8f90e753
Ifeoluwa <sn035770@firstbanknigeria.com>
2026-02-08 15:23:37 +0100

    love commit

---
+++ config.js
@@ -0,0 +1,50 @@
+const CONFIG = {
+  // Couple Information
+  couple: {
+    name1: 'My Lovely Okikiniwura',  // The main name displayed
+    name2: '',                        // Left empty
+    connector: '',                    // Removed connector
+    together: ''                      // Removed "together" text
+  },
+  
+  // Memorial Date 
+  // (We will hide the clock display in CSS, so this date won't be seen)
+  memorialDate: '2023-01-01T00:00:00',
+  
+  // Love Letter Content
+  letter: {
+    paragraph1: [
+      'My Dearest Okikiniwura,',
+      'You are the most precious treasure in my life.',
+      'Every moment with you feels like a beautiful dream,',
+      'and I find myself falling in love with you',
+      'more and more each day.'
+    ],
+    paragraph2: [
+      'Your smile lights up my world in ways',
+      'I never imagined possible.',
+      'You are my joy, my peace, and my everything.',
+      'I cherish every laugh, every silence,',
+      'and every memory we create together.'
+    ],
+    paragraph3: [
+      'I promise to stand by you, to honor you,',
+      'and to love you through all of life\'s seasons.',
+      'You are my heart\'s eternal home.',
+      ' ',
+      'Love, Ifeoluwakiitan ❤️'
+    ]
+  },
+  
+  // Time Display Text (Hidden by CSS, but kept here to prevent errors)
+  time: {
+    prefix: '',
+    day: 'days',
+    hour: 'hours',
+    minute: 'min',
+    second: 'sec'
+  },
+  
+  // Text on seed heart
+  seedText: 'For You'
+};
